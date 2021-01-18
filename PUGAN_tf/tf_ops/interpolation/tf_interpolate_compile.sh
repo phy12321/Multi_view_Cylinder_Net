52c305437c2fa72dc2eb3bde93644955b0016455
phy12321 <1079677436@qq.com>
2021-01-18 16:40:45 +0800

    delet pugan module

--- PUGAN_tf/tf_ops/interpolation/tf_interpolate_compile.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-#g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I /home/phy12321/anaconda3/envs/tensorflow3/lib/python3.6/site-packages/tensorflow/include  -I /usr/local/cuda/include -lcudart -L /usr/local/cuda/lib64/ -O2 -D_GLIBCXX_USE_CXX11_ABI=0
-#g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I /home/phy12321/anaconda3/envs/tensorflow3/lib/python2.7/site-packages/tensorflow/include  -I /usr/local/cuda/include -lcudart -L /usr/local/cuda/lib64/ -O2 -D_GLIBCXX_USE_CXX11_ABI=0
-
-g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I /home/phy12321/anaconda3/envs/pugan/lib/python3.6/site-packages/tensorflow/include  -I /usr/local/cuda/include -I /home/phy12321/anaconda3/envs/pugan/lib/python3.6/site-packages/tensorflow/include/external/nsync/public -lcudart -L /usr/local/cuda/lib64/ -L/home/phy12321/anaconda3/envs/pugan/lib/python3.6/site-packages/tensorflow -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
-
--- PUGAN_tf/tf_ops/sampling/tf_sampling_compile.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-#/bin/bash
-/usr/local/cuda/bin/nvcc tf_sampling_g.cu -o tf_sampling_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
-#g++ -std=c++11 tf_sampling.cpp tf_sampling_g.cu.o -o tf_sampling_so.so -shared -fPIC -I /data/phy12321/anaconda3/envs/tensorflow3/lib/python3.6/site-packages/tensorflow/include  -I /usr/local/cuda/include -lcudart -L /usr/local/cuda/lib64/ -O2 -D_GLIBCXX_USE_CXX11_ABI=0
-g++ -std=c++11 tf_sampling.cpp tf_sampling_g.cu.o -o tf_sampling_so.so -shared -fPIC -I /home/phy12321/anaconda3/envs/pugan/lib/python3.6/site-packages/tensorflow/include  -I /usr/local/cuda/include -I /home/phy12321/anaconda3/envs/pugan/lib/python3.6/site-packages/tensorflow/include/external/nsync/public -lcudart -L /usr/local/cuda/lib64/ -L/home/phy12321/anaconda3/envs/pugan/lib/python3.6/site-packages/tensorflow -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
